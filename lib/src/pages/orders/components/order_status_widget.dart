ce0db99fcace66de1b2bcbef7cf954caf428d83e
Giovani <giovani.silva@guardiao.digital>
2024-07-01 12:33:52 -0300

    conitnuando tela de pedidos

--- lib/src/pages/orders/components/order_status_widget.dart
+++ lib/src/pages/orders/components/order_status_widget.dart
@@ -5,7 +5,18 @@ class OrderStatusWidget extends StatelessWidget {
   final String status;
   final bool isOverdue;
 
-  const OrderStatusWidget({
+  final Map<String, int> allStatus = <String, int>{
+    'pending_payment': 0,
+    'refunded': 1,
+    'paid': 2,
+    'preparing_purchase': 3,
+    'shipping': 4,
+    'delivered': 5,
+  };
+
+  int get currentStatus => allStatus[status]!;
+
+  OrderStatusWidget({
     super.key,
     required this.status,
     required this.isOverdue,
@@ -13,17 +24,50 @@ class OrderStatusWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _StatusDot(
-          tile: 'Teste de pagamento',
+        const _StatusDot(
           isActive: true,
+          tile: 'Pedido confirmado',
         ),
-        _StatusDot(
-          tile: 'Pagamento Efetuado',
-          isActive: false,
-        ),
+        const _CustomDivider(),
+        if (currentStatus == 1) ...[
+          const _StatusDot(
+            isActive: true,
+            tile: 'PIX estornado',
+            backgroundColor: Colors.orange,
+          )
+        ] else if (isOverdue) ...[
+          const _StatusDot(
+            isActive: true,
+            tile: 'Pagamento PIX vencido',
+            backgroundColor: Colors.red,
+          )
+        ] else ...[
+          _StatusDot(isActive: currentStatus >= 2, tile: 'Pagamento'),
+          const _CustomDivider(),
+          _StatusDot(isActive: currentStatus >= 3, tile: 'Preparando'),
+          const _CustomDivider(),
+          _StatusDot(isActive: currentStatus >= 4, tile: 'Envio'),
+          const _CustomDivider(),
+          _StatusDot(isActive: currentStatus >= 5, tile: 'Entregue'),
+        ],
       ],
+    );
+  }
+}
+
+class _CustomDivider extends StatelessWidget {
+  const _CustomDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+      height: 10,
+      width: 2,
+      color: Colors.grey.shade300,
     );
   }
 }
@@ -31,11 +75,13 @@ class OrderStatusWidget extends StatelessWidget {
 class _StatusDot extends StatelessWidget {
   final bool isActive;
   final String tile;
+  final Color? backgroundColor;
 
   const _StatusDot({
     super.key,
     required this.isActive,
     required this.tile,
+    this.backgroundColor,
   });
 
   @override
@@ -52,8 +98,9 @@ class _StatusDot extends StatelessWidget {
             border: Border.all(
               color: CustomColors.customSwatchColor,
             ),
-            color:
-                isActive ? CustomColors.customSwatchColor : Colors.transparent,
+            color: isActive
+                ? backgroundColor ?? CustomColors.customSwatchColor
+                : Colors.transparent,
           ),
           child: isActive
               ? const Icon(
@@ -69,7 +116,11 @@ class _StatusDot extends StatelessWidget {
         ),
 
         // Texto
-        Expanded(child: Text(tile)),
+        Expanded(
+            child: Text(
+          tile,
+          style: const TextStyle(fontSize: 12),
+        )),
       ],
     );
   }
