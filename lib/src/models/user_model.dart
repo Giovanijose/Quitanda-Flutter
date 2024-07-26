303d181d0486e5684e43017fb03490035827a197
Giovani <giovani.silva@guardiao.digital>
2024-07-26 14:12:58 -0300

    requests

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,17 +1,35 @@
-class UserModel{
+import 'package:json_annotation/json_annotation.dart';
 
-  String name;
-  String email;
-  String phone;
-  String cpf;
-  String password;
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+  @JsonKey(name: 'fullname')
+  String? name;
+  String? email;
+  String? phone;
+  String? cpf;
+  String? password;
+  String? id;
+  String? token;
 
   UserModel({
-    required this.name,
-    required this.email,
-    required this.phone,
-    required this.cpf,
-    required this.password,
+    this.name,
+    this.email,
+    this.phone,
+    this.cpf,
+    this.password,
+    this.id,
+    this.token,
   });
 
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+  @override
+  String toString() {
+    return 'name: $name | cpf: $cpf';
+  }
 }
