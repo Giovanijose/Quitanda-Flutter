b51a8f7178aedac92e38dcfed8205ac94d968666
Giovani <giovani.silva@guardiao.digital>
2024-07-29 13:59:29 -0300

    result request

--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -1,21 +1,33 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/user_model.dart';
+import 'package:greengrocer/src/pages/auth/repository/auth_errors.dart';
+import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
-  Future signIn({required String email, required String password}) async {
+  Future<AuthRepository> validateToken(String token) async {
+    final result = await _httpManager.restRequest(
+        url: Endpoints.validateToken,
+        method: HttpMethods.post,
+        headers: {'X-Parse-Session-Token': token});
+    return result;
+  }
+
+  Future<AuthResult> signIn(
+      {required String email, required String password}) async {
     final result = await _httpManager.restRequest(
       url: Endpoints.signin,
       method: HttpMethods.post,
-      body: {"email": email, "password": password},
+      body: {'email': email, 'password': password},
     );
 
     if (result['result'] != null) {
       final UserModel user = UserModel.fromJson(result['result']);
-
-      print(user);
-    } else {}
+      return AuthResult.success(user);
+    } else {
+      return AuthResult.error(authErrorsString(result['error']));
+    }
   }
 }
