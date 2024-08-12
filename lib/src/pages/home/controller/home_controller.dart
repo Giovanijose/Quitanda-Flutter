51c865fafdd054eb5bd84c7ba227f1910e2a4e99
Giovani <giovani.silva@guardiao.digital>
2024-08-12 14:04:19 -0300

    categories

---
+++ lib/src/pages/home/controller/home_controller.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/pages/home/repository/home_repository.dart';
+import 'package:greengrocer/src/pages/home/result/home_result.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
+
+class HomeController extends GetxController {
+  final homeRepository = HomeRepository();
+  final utilsServices = UtilsServices;
+
+  bool isLoading = false;
+  List<CategoryModel> allCategories = [];
+
+  void setLoading(bool value) {
+    isLoading = value;
+    update();
+  }
+
+  Future<void> getAllCategories() async {
+    setLoading(true);
+    HomeResult<CategoryModel> homeResult =
+        await homeRepository.getAllCategories();
+    setLoading(false);
+
+    homeResult.when(
+        success: (data) {
+          allCategories.assignAll(data);
+        },
+        error: (message) {});
+  }
+}
