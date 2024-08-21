c6e74e1df3e7bea9f9829f3144288bf890ca19d5
Giovani <giovani.silva@guardiao.digital>
2024-08-21 13:48:26 -0300

    navigation

--- lib/src/pages/base/controller/navigation_controller.dart
+++ lib/src/pages/base/controller/navigation_controller.dart
@@ -2,12 +2,30 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
+abstract class NavigationTabs {
+  static const int home = 0;
+  static const int cart = 1;
+  static const int orders = 2;
+  static const int profile = 3;
+}
+
 class NavigationController extends GetxController {
   late PageController _pageController;
-
   late RxInt _currentIndex;
 
-  void initNavigation({
+  PageController get pageController => _pageController;
+  int get currentIndex => _currentIndex.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _initNavigation(
+      pageController: PageController(initialPage: NavigationTabs.home),
+      currentIndex: NavigationTabs.home,
+    );
+  }
+
+  void _initNavigation({
     required PageController pageController,
     required int currentIndex,
   }) {
@@ -16,7 +34,16 @@ class NavigationController extends GetxController {
   }
 
   void navigatePageView(int page) {
-    _pageController.jumpToPage(page);
+    if (_currentIndex.value == page) {
+      return;
+    }
+
+    // _pageController.jumpToPage(page);
+    _pageController.animateToPage(
+      currentIndex,
+      duration: const Duration(milliseconds: 700),
+      curve: Curves.ease,
+    );
     _currentIndex.value = page;
   }
 }
